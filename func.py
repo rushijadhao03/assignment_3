9d4d06ffa78a738e25d6c2417869fcb42dde46c6
rushijadhao03 <rushikeshjadhao29@gmail.com>
2025-07-20 15:39:31 +0530

    Add files via upload

---
+++ func.py
@@ -0,0 +1,8 @@
+n=int(input("enter a number:"))
+def factorial(n):
+    if n<2:
+        return 1
+    else:
+        return n*factorial(n-1)
+result=factorial(n)
+print("The factorial of",n, " is:",result)
